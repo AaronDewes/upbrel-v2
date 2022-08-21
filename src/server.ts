08c45c210668e135c199d0b4e66f42d86efaba4c
AaronDewes <aaron.dewes@protonmail.com>
2022-08-21 16:55:25 +0000

    Add cache

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,51 @@
 import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
 import { getAppUpgrades } from "./appcheck.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+import { connect } from "https://deno.land/x/redis@v0.26.0/mod.ts";
 
 const isDenoDeploy = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
 
-if (!isDenoDeploy)
-    config();
-
-serve(async (_req: Request) => new Response(
-    JSON.stringify(await getAppUpgrades()), {
-    headers: { "content-type": "application/json", "access-control-allow-origin": "*" },
+if (!isDenoDeploy) {
+  config();
 }
-));
+
+const cache = await connect({
+  name: Deno.env.get("REDIS_DATABASE"),
+  port: Number(Deno.env.get("REDIS_PORT")),
+  hostname: Deno.env.get("REDIS_HOST") as string,
+  username: Deno.env.get("REDIS_USERNAME"),
+  password: Deno.env.get("REDIS_PASSWORD"),
+});
+
+serve(async (_req: Request) => {
+  try {
+    const cached = await cache.get("available_updates");
+    if (!cached) {
+      throw new Error("No cache");
+    }
+    console.log("Cache found!");
+
+    return new Response(
+      cached,
+      {
+        headers: {
+          "content-type": "application/json",
+          "access-control-allow-origin": "*",
+        },
+      },
+    );
+  } catch {
+    console.log("No cache!");
+    const result = JSON.stringify(await getAppUpgrades());
+    await cache.set("available_updates", result);
+    return new Response(
+      result,
+      {
+        headers: {
+          "content-type": "application/json",
+          "access-control-allow-origin": "*",
+        },
+      },
+    );
+  }
+});
