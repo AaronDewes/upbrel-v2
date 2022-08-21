2f6cdac03c663c52cb46189146fc45d9098106a6
AaronDewes <aaron.dewes@protonmail.com>
2022-08-21 10:52:30 +0000

    Fix for deno deploy

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
 import { getAppUpgrades } from "./appcheck.ts";
 import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-config();
+
+const isDenoDeploy = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
+
+if (isDenoDeploy)
+    config();
 
 serve(async (_req: Request) => new Response(
     JSON.stringify(await getAppUpgrades()), {
