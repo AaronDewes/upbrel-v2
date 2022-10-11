aafe124c3006b72dcf77ef854602aac473495190
AaronDewes <aaron.dewes@protonmail.com>
2022-10-11 10:39:32 +0000

    Use CSS for scrolling

--- routes/index.tsx
+++ routes/index.tsx
@@ -43,7 +43,7 @@ export default function MainPage(props: PageProps<MainData>) {
         />
         <link rel="stylesheet" href="/index.css" />
       </Head>
-      <div class="text-white scroll-smooth">
+      <div class="text-white">
         <main class="relative h-screen flex flex-col items-center justify-center">
           <h1 class="text-9xl">{props.data.percentageOutdated}%</h1>
           <p>
