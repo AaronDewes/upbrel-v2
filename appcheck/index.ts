471e42675f5082e099ce6ab75bd7c82305be80f2
Aaron Dewes <aaron.dewes@protonmail.com>
2023-10-21 22:55:11 +0200

    Update index.ts

--- appcheck/index.ts
+++ appcheck/index.ts
@@ -11,29 +11,23 @@ if (!isDenoDeploy) {
   config();
 }
 
-const cache = await connect({
-  name: Deno.env.get("REDIS_DATABASE"),
-  port: Number(Deno.env.get("REDIS_PORT")),
-  hostname: Deno.env.get("REDIS_HOST") as string,
-  username: Deno.env.get("REDIS_USERNAME"),
-  password: Deno.env.get("REDIS_PASSWORD"),
-});
+const cache = await Deno.openKv();
 
 export async function getAppUpgrades(cached = true): Promise<updateInfo> {
   try {
     if (!cached) {
       throw new Error("No cache");
     }
-    const cachedData = await cache.get("available_updates");
+    const cachedData = await cache.get(["available_updates"]);
     if (!cachedData) {
       throw new Error("No cache");
     }
 
-    return JSON.parse(cachedData);
+    return cachedData;
   } catch {
     const data = await _checkUpdates();
-    await cache.set("available_updates", JSON.stringify(data), {
-      ex: 60 * 5,
+    await cache.set(["available_updates"], data, {
+      expireIn: 60 * 5,
     });
     return data;
   }
