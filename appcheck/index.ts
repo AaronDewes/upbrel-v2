a65673538798e1fe602b6f438a06143b81722cd4
Aaron Dewes <aaron.dewes@protonmail.com>
2023-10-21 23:06:02 +0200

    Update index.ts

--- appcheck/index.ts
+++ appcheck/index.ts
@@ -1,15 +1,7 @@
-9import {
+import {
   getAppUpgrades as _checkUpdates,
   type updateInfo,
 } from "./appcheck.ts";
-import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-import { connect } from "https://deno.land/x/redis@v0.26.0/mod.ts";
-
-const isDenoDeploy = Deno.env.get("DENO_DEPLOYMENT_ID") !== undefined;
-
-if (!isDenoDeploy) {
-  config();
-}
 
 const cache = await Deno.openKv();
 
